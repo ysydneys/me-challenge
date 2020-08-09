25eb24dd8225f91fd390c09c0c0f77a1266bc92a
James H <ysydneys@gmail.com>
2020-08-09 20:05:06 +1000

    created retrofit service interface

---
+++ app/src/main/java/com/ysydneys/meapplication/api/ApiConfig.kt
@@ -0,0 +1,3 @@
+package com.ysydneys.meapplication.api
+
+const val EMPLOYEE_API_ENDPOINT = "http://10.0.2.2:5000"
---
+++ app/src/main/java/com/ysydneys/meapplication/api/EmployeeService.kt
@@ -0,0 +1,14 @@
+package com.ysydneys.meapplication.api
+
+import com.ysydneys.meapplication.model.Employee
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface EmployeeService {
+
+    @GET("/employees")
+    suspend fun getEmployeeList(): List<Employee>
+
+    @GET("/employees/{employee_id}")
+    suspend fun getEmployeeDetail(@Path("employee_id") employeeId: Int): Employee
+}
