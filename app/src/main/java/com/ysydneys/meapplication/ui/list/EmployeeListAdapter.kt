44e396d94d37a50898d382be49d011443b2a3e5d
James H <ysydneys@gmail.com>
2020-08-09 21:32:26 +1000

    display thumbnail images

--- app/src/main/java/com/ysydneys/meapplication/ui/list/EmployeeListAdapter.kt
+++ app/src/main/java/com/ysydneys/meapplication/ui/list/EmployeeListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.ysydneys.meapplication.R
 import com.ysydneys.meapplication.databinding.ItemEmployeeBinding
 import com.ysydneys.meapplication.model.Employee
@@ -33,6 +34,9 @@ class EmployeeListAdapter (val onClick: (Int) -> Unit) : RecyclerView.Adapter<Em
 
     override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
         holder.itemEmployeeBinding.employeeItem = mEmployeeList[position]
+
+        val data = android.util.Base64.decode(mEmployeeList[position].thumbImage, android.util.Base64.DEFAULT)
+        Glide.with(holder.itemEmployeeBinding.thumbNailImage.context).load(data).fitCenter().placeholder(R.drawable.ic_gender_24).into(holder.itemEmployeeBinding.thumbNailImage)
 
         holder.itemEmployeeBinding.containerLayout.setOnClickListener {
             onClick(mEmployeeList[position].id)
